96205349cb7284a636710d39dc430bfd6f3095af
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-25 12:33:18 -0700

    exercise 5 completed

--- ex05.js
+++ ex05.js
@@ -1,3 +1,13 @@
 // Some Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if some of the numbers are even.
 
+const numbers = [11, 77, 9, 5, 83, 41, 54];
+const isThereAnEvenNumber = numbers.some((numbers) => {
+  return numbers % 2 === 0;
+});
+
+if (isThereAnEvenNumber) {
+  console.log("Some of the numbers are even");
+} else {
+  console.log("Non of the numbers are even");
+}
