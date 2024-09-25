fb534fc28ba8d0fa74d752aa9d8936d9c48a8a3f
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-25 11:47:07 -0700

    exercise 3 completed

--- ex03.js
+++ ex03.js
@@ -1,3 +1,8 @@
 // Reverse Exercise
 // Instructions: Create an array of at least five numbers. Write code to reverse the order of the elements in the array.
 
+const numbers = [12, 87, 37, 34, 884, 74];
+const reversedNum = [...numbers];
+reversedNum.reverse();
+
+console.log("COPIED", reversedNum);
