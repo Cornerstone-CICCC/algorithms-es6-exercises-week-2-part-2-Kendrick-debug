8037a0d00524f9589c1900fb288ad124f1d25d0d
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-25 12:34:35 -0700

    exercise 8 completed

--- ex08.js
+++ ex08.js
@@ -1,3 +1,6 @@
 // Join Exercise
 // Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
 
+const words = ["The", "waether", "is", "very", "cold", "today"];
+
+console.log(words.join(" "));
