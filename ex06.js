6e00d91bd75cdd0d8ad85cfe1b0d68234b9c8ed9
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-25 12:36:18 -0700

    exercise 6 completed

--- ex06.js
+++ ex06.js
@@ -1,3 +1,13 @@
 // Every Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if every number in the array is positive.
 
+const numbers = [48, 3, 9, 5, 91, 31, 74];
+const isEveryNumPositive = numbers.every((number) => {
+  return number > 0;
+});
+
+if (isEveryNumPositive) {
+  console.log("Every number is positive");
+} else {
+  console.log("Not all the nnumbers are positive");
+}
